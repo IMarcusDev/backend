50edd91abde2aa43b2fa574635cea5ab7c16bde2
IMarcusDev <escobarmarcos511@gmail.com>
2025-02-18 17:58:29 -0500

    Produccion v0.2

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 import mysql from "mysql2/promise";
+import {
+  DB_NAME,
+  DB_PORT,
+  DB_PASSWORD,
+  DB_USER,
+  DB_HOST
+} from './config.js'
 
-// Configuración de la conexión a la base de datos en InfinityFree
 const pool = mysql.createPool({
-  host: "sql312.infinityfree.com", // Host de InfinityFree
-  user: "if0_38347125", // Usuario de la base de datos
-  password: "LVsN1qbiqWM", // Contraseña (⚠️ Mejor usar variables de entorno en producción)
-  database: "if0_38347125_consultorio_db", // Nombre correcto de la base de datos
-  port: 3306, // Puerto de MySQL en InfinityFree
+  host: DB_HOST,
+  user: DB_USER, 
+  password: DB_PASSWORD, 
+  database: DB_NAME, 
+  port: DB_PORT, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- server.js
+++ server.js
@@ -1,37 +1,26 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-import * as Queries from "./db/queries.js"; // Importación de queries
+import * as Queries from "./db/queries.js"; // 
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Usar puerto dinámico en producción
-// Obtener __dirname en ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 5000; 
 
-// Middleware
 app.use(cors({
-  origin: "https://IMarcusDev.github.io/ProjectConsultorio", // Reemplaza con la URL de tu GitHub Pages
+  origin: "https://IMarcusDev.github.io/ProjectConsultorio",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 app.use(express.json()); 
 
-// Revisar localhost:${PORT}/api/status para ver detalles de conexión
 app.get("/api/status", async (req, res) => {
   try {
-    const rows = await Queries.getRoles();  // Roles ya tiene valores por defecto
+    const rows = await Queries.getRoles();
 
     res.json({ message: "Conexión a la base de datos exitosa" });
   } catch (error) {
     res.status(500).json({ message: "Error al conectar con la base de datos", error: error.message });
   }
 });
-
-// Demás funciones aquí
-// Crear todas las solicitudes llamando desde /api/dataToFind
-// Ej: app.get("/api/citas/doc/:id_doc", ...), req.params.id_doc toma el parámetro id_doc
 
 app.get("/api/user/:username", async (req, res) => {
   try {
@@ -42,10 +31,6 @@ app.get("/api/user/:username", async (req, res) => {
       res.status(500).json({ message: error.message });
   }
 });
-
-//logica
-
-//aqui defino como hago yo /api/pongo el nombre yo , req es la solicitud que le hago al front para que me pase los parametros
 
 app.post("/api/register", async (req, res) => {
   try {
@@ -548,19 +533,5 @@ app.post("/api/ListaDoctoresSecretarios", async (req, res) => {
 });
 
 
-
-// Servir el frontend en producción
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-  });
-}
-
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT} o en Railway`);
-});
-
+app.listen(PORT);
 
